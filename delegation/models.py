3ae2ce1845848596c861ee217fa369f6ddc05b09
daniilbayrachnyi <dbayrachnyi@zerosystems.com>
2024-12-15 11:43:25 +0400

    feat(delegation): delegation table models

--- delegation/models.py
+++ delegation/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Q
 
-# Create your models here.
+DELEGATION_SCHEMA = "delegation"
+
+
+class UserActor(models.Model):
+    class Meta:
+        db_table = f"{DELEGATION_SCHEMA}.user_actor"
+        constraints = [models.CheckConstraint(check=Q(period_end__gt="period_start"), name="period_end_start_check")]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    actor = models.ForeignKey("billing.Actor", on_delete=models.CASCADE)
+    period_start = models.DateTimeField()
+    period_end = models.DateTimeField()
